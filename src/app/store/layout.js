53852e50c40da63b32730b0ea7400f76b359ee85
gyongcode <gyongcode@gmail.com>
2025-05-15 23:44:01 +0900

    store commit

---
+++ src/app/store/layout.js
@@ -0,0 +1,34 @@
+import PageMenu from "@/components/store/PageMenu";
+import SiteContainer from "@/components/store/SiteContainer";
+
+export default function RootLayout({ children }) {
+  const items = [
+    {
+      href: "/store/map",
+      label: "매장 찾기",
+    },
+    {
+      href: "/store/flavor",
+      label: "100 flavor",
+    },
+    {
+      href: "/store/workshop",
+      label: "Workshop",
+    },
+    {
+      href: "/store/delivery",
+      label: "배달주문",
+    },
+    {
+      href: "/store/catering",
+      label: "단체주문",
+    },
+  ];
+
+  return (
+    <SiteContainer>
+      <PageMenu items={items} />
+      {children}
+    </SiteContainer>
+  );
+}
