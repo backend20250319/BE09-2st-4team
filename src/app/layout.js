db9ad84c26a3911b7cc9ad905807b0f056dc6f51
juhui <juya202106@naver.com>
2025-05-16 14:26:08 +0900

    header 대문자 바꿈

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/mainpage/main.css";
-import "../styles/header.css";
+import "../styles/Header.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
