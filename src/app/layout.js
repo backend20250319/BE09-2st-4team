6a407b3b863f989ce11d5280bb56ec9cbfe784fa
junbem <junbem0728@naver.com>
2025-05-16 17:04:48 +0900

    error catch

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,16 +3,6 @@ import "@/styles/Header.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "배스킨라빈스",
   description: "프리미엄 아이스크림 브랜드",
@@ -25,7 +15,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=1280, user-scalable=no" />
       </head>
 
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Header />
         <main
           style={{ paddingTop: "90px", minHeight: "60vh", background: "#fff" }}
