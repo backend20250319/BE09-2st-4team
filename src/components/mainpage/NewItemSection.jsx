570f5208701e115de6e91f14d08c047cffd4a0bd
juhui <juya202106@naver.com>
2025-05-18 23:12:20 +0900

    newitem의 text부분 오타 수정, swiper 설치

--- src/components/mainpage/NewItemSection.jsx
+++ src/components/mainpage/NewItemSection.jsx
@@ -2,12 +2,12 @@
 import "@/styles/mainpage/newitem.css";
 
 export default function NewItemSection() {
-  // 신제품 데이터
+
   const newItems = [
     {
       id: 1,
       image: "/images/main/new_item1.png",
-      isLarge: true, // 큰 사이즈 아이템
+      isLarge: true,
     },
     {
       id: 2,
@@ -28,11 +28,11 @@ export default function NewItemSection() {
   ];
 
   const itemTexts = [
-    "", // 큰 이미지는 텍스트 div가 없음
+    "",
     "플라워 블라썸 케이크",
     "플라워 베리 케이크",
-    "감사를 담은 레이블낫\n초코딸기 딸기바다",
-    "사랑을 담은 유자\n마스카포네 딸기바다",
+    "감사를 담은 헤이즐넛\n초콜릿칩 달항아리",
+    "사랑을 담은 유자\n마스카포네 달항아리",
   ];
 
   return (
@@ -45,7 +45,6 @@ export default function NewItemSection() {
       </div>
 
       <div className="newitem-grid">
-        {/* 큰 아이템(I WANT CHU!) - 이미지만 */}
         <div className="newitem-card-large">
           <img
             src={newItems[0].image}
@@ -54,15 +53,12 @@ export default function NewItemSection() {
           />
         </div>
 
-        {/* 나머지 아이템들 - 이미지 + 텍스트 div */}
         {newItems.slice(1).map((item, index) => (
           <div key={item.id} className="newitem-item-container">
-            {/* 이미지 부분 */}
             <div className="newitem-image-container">
               <img src={item.image} alt="" className="newitem-product-image" />
             </div>
 
-            {/* 텍스트 div 부분 */}
             <div className="newitem-text-container">
               <p className="newitem-product-title">{itemTexts[index + 1]}</p>
               <div className="newitem-arrow-circle">
