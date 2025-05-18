6a134a69864c0c4acd83bf42e3d8d65cb4b34c68
juhui <juya202106@naver.com>
2025-05-18 22:55:49 +0900

    brbenefit 이미지 연동과 함께 구현 완료

--- src/components/mainpage/BrBenefit.jsx
+++ src/components/mainpage/BrBenefit.jsx
@@ -1,8 +1,35 @@
-// BRBenefitSection.jsx
 "use client";
 import "@/styles/mainpage/brbenefit.css";
 
 export default function BRBenefitSection() {
+  const benefitItems = [
+    {
+      id: 1,
+      image: "/images/main/benefit1.png",
+      alt: "유플러스 Pick 2",
+    },
+    {
+      id: 2,
+      image: "/images/main/benefit2.png",
+      alt: "Samsung Card",
+    },
+    {
+      id: 3,
+      image: "/images/main/benefit3.png",
+      alt: "Hyundai Card Apple Pay",
+    },
+    {
+      id: 4,
+      image: "/images/main/benefit4.png",
+      alt: "Happy Point",
+    },
+    {
+      id: 5,
+      image: "/images/main/benefit5.png",
+      alt: "M Point",
+    },
+  ];
+
   return (
     <div className="brbenefit-container">
       <div className="brbenefit-header">
@@ -13,12 +40,17 @@ export default function BRBenefitSection() {
       </div>
 
       <div className="brbenefit-content">
-        {/* 나중에 스위퍼로 구현할 부분 */}
-        <img
-          src="/images/main/br_benefit_carousel.jpg"
-          alt="BR Benefit 이벤트"
-          className="brbenefit-carousel-image"
-        />
+        <div className="brbenefit-grid">
+          {benefitItems.map((item) => (
+            <div key={item.id} className="brbenefit-card">
+              <img
+                src={item.image}
+                alt={item.alt}
+                className="brbenefit-card-image"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
